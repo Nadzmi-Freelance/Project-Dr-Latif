b545ba20b3ee570a2c5ceacaed6db5dc608a4311
Seladang Hijau <nadzmiidzham@gmail.com>
2016-04-10 07:47:14 +0800

    Update 2, Test 3: Reconstruct folder
    Created folder for all PHP & SQL files

---
+++ PHP & SQL/connect.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$username = "";
+$password = "";
+$dbname = "uitmkedah_boook_db";
+
+$conn = mysqli_connect($host, $username, $password, $dbname) or die(mysql_error());
+?>
